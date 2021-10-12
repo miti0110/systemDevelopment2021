45589cf61c501256d812c0fa568a137eb56d0998
miti0110 <87954567+miti0110@users.noreply.github.com>
2021-10-12 11:57:38 +0900

    Add files via upload

---
+++ java/src/java/cat.java
@@ -0,0 +1,27 @@
+package java;
+import java.io.*;
+
+class Cat {
+    public static void main(String args[]) {
+        if (args.length != 1) {
+            System.exit(1);
+        }
+
+        String path = args[0];
+        try {
+            File file = new File(path);
+            FileReader reader = new FileReader(file);
+
+            int ch = reader.read();
+            while (ch != -1) {
+                System.out.print((char)ch);
+                ch = reader.read();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+}
